86ed2352fb8ad1db309393ccc867b2b490bb33de
HRFernandezEpidata <81421817+HRFernandezEpidata@users.noreply.github.com>
2021-12-21 10:08:13 -0300

    Add files via upload

--- ReportReleases.ps1
+++ ReportReleases.ps1
@@ -1,4 +1,4 @@
-﻿param ($organization, $project, $token)
+﻿param ($organization, $project, $token, $pipeline)
 
 # Base64-encodes the Personal Access Token (PAT) appropriately
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes((":{0}" -f $token)))
@@ -7,11 +7,9 @@ $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes((":{
 $apiURL = "https://vsrm.dev.azure.com/$organization/$project/_apis/release/releases?api-version=6.0"
 
 #Request           
-
 $response = Invoke-RestMethod -Uri $apiURL -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} 
 
 $releases = $response.value
-
 
 $dataReleases = @()
 
@@ -26,6 +24,39 @@ $results = @()
 foreach($release in $dataReleases){
 
     foreach($stage in $release.environments){
+
+        ##Approvals control
+        $approvals = "";
+        foreach($approval in $stage.preDeployApprovals){
+            
+            if ($approval.approver){
+                $approvals += $approval.approver.uniqueName 
+                if ($approval.approvedBy){
+                    $approvals += "(Approved) "
+                }
+                else
+                {
+                    $approvals += "(Pending) "
+                }
+            }
+            else {
+                $approvals += "(None)"
+            }
+        }
+
+        if ($approvals -eq "") {
+            $approvals += "(None)"
+        }
+
+        ##CreatedOn control
+        $createdOn = ""
+        if ($stage.createdOn) {
+             $createdOn += $stage.createdOn
+        }
+        else {
+            $createdOn += "(None)"
+        }
+
         $properties = [ordered]@{
           Pipeline    = $release.releaseDefinition.name
           Release     = $release.name
@@ -33,31 +64,35 @@ foreach($release in $dataReleases){
           Stage = $stage.name
           StageStatus = $stage.status
           StageId = $stage.id
-          CreatedOn = $stage.createdOn 
+          CreatedOn = $createdOn
+          Approvals = $approvals
         }
 
         $customObject = New-Object -TypeName PSCustomObject -Property $properties
 
         $results += $customObject
-
     }
-           
 }
 
+
+## Order the releases
 $results = $results | Sort-Object -Property @{Expression = "Pipeline"}, 
                                             @{Expression = "ReleaseId"; Descending = $False},
                                             @{Expression = "StageId"; Descending = $False}
 
+## Filter by pipeline name
+$resultsFiltered = $results | ?{ $_.Pipeline -eq "New release pipeline" }
+
+## File creation
 $date = Get-Date -Format G
 $dateFormated = $date.Replace("/", "-").Replace(":", ".")
-$fileName = "Releases $dateFormated.csv"
+$fileName = "$pipeline Releases $dateFormated.csv"
 
-$results | Select `
+$resultsFiltered | Select `
             Pipeline,
+            ReleaseId,
             Release,
             Stage,
             StageStatus,
-            CreatedOn | export-csv -Path $fileName -NoTypeInformation
-
-
-##qgiplmj4bufpoxympzdwfjcgtzsvd3l455ck3eok2gkxefpgy52q
+            CreatedOn,
+            Approvals | export-csv -Path $fileName -NoTypeInformation
