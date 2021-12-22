6f355bde8af8c4b48e89221606327e6fad5c6cae
HRFernandezEpidata <81421817+HRFernandezEpidata@users.noreply.github.com>
2021-12-22 08:23:48 -0300

    Update script

--- ReportReleases.ps1
+++ ReportReleases.ps1
@@ -1,78 +1,128 @@
 ﻿param ($organization, $project, $token, $pipeline)
 
-# Base64-encodes the Personal Access Token (PAT) appropriately
+## Base64-encodes the Personal Access Token (PAT) appropriately
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes((":{0}" -f $token)))
 
-#BaseUrl
-$apiURL = "https://vsrm.dev.azure.com/$organization/$project/_apis/release/releases?api-version=6.0"
+## We list the pipelines to get the id of the pipeline passed in the argument
+Write-Host "Getting pipeline id '$pipeline'..."
 
-#Request           
-$response = Invoke-RestMethod -Uri $apiURL -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} 
+$pipelinesURL =  "https://vsrm.dev.azure.com/$organization/$project/_apis/release/definitions?`$top=100&searchText=$pipeline&api-version=6.0"
 
-$releases = $response.value
+try {
+    $response = Invoke-WebRequest -Uri $pipelinesURL -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
 
-$dataReleases = @()
-
-foreach($release in $releases){
-    $dataRelease= Invoke-RestMethod -Uri $release.url -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} 
-    $dataReleases += $dataRelease
+    if($response.statusCode -eq 203) {
+        Write-Error "Error 203 'Non-Authoritative Information'. The token entered is invalid, verify that it is not expired or that you have the required permissions."
+        Exit
+    }
+}
+catch [System.Net.WebException] {
+    if ($_.Exception.Response.StatusCode -eq "NotFound") {
+        Write-Error "Resource not found verify organization and project name is correct."
+        Exit
+    }
 }
 
+$content = $response.Content | ConvertFrom-Json
+
+$pipelines = $content.value
+
+$idPipeline
+foreach($p in $pipelines) {
+    if ($p.name -eq $pipeline) {
+        $idPipeline = $p.id
+    }
+}
+
+if($idPipeline -eq $null) {
+    Write-Error "Could not find the pipeline with the name '$pipeline'."
+    Exit
+}
+
+Write-Host "Pipeline id found: $idPipeline."
+
+## We obtain the releases using the pipeline id.
+Write-Host "Getting pipeline releases with id $idPipeline..."
+$continuationToken = 0
+
+$releasesURL = "https://vsrm.dev.azure.com/$organization/$project/_apis/release/releases?`$top=100&definitionId=$idPipeline&api-version=6.0"
 
 $results = @()
 
-foreach($release in $dataReleases){
+do {
+    $response = Invoke-WebRequest -Uri $releasesURL -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
 
-    foreach($stage in $release.environments){
+    $continuationToken = $response.headers["x-ms-continuationtoken"]
 
-        ##Approvals control
-        $approvals = "";
-        foreach($approval in $stage.preDeployApprovals){
+    $releasesURL = "https://vsrm.dev.azure.com/$organization/$project/_apis/release/releases?`$top=100&continuationToken=$continuationToken&definitionId=$idPipeline&api-version=6.0"
+
+    $content = $response.Content | ConvertFrom-Json
+
+    $releases = $content.value
+
+    $dataReleases = @()
+
+    foreach($release in $releases){
+        $dataRelease= Invoke-RestMethod -Uri $release.url -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} 
+        $dataReleases += $dataRelease
+    }
+
+
+    foreach($release in $dataReleases) {
+        foreach($stage in $release.environments) {
+        
+            ##Approvals control
+            $approvals = "";
+            foreach($approval in $stage.preDeployApprovals){
             
-            if ($approval.approver){
-                $approvals += $approval.approver.uniqueName 
-                if ($approval.approvedBy){
-                    $approvals += "(Approved) "
+                if ($approval.approver){
+                    $approvals += $approval.approver.uniqueName 
+                    if ($approval.approvedBy){
+                        $approvals += "(Approved) "
+                    }
+                    else
+                    {
+                        $approvals += "(Pending) "
+                    }
                 }
-                else
-                {
-                    $approvals += "(Pending) "
+                else {
+                    if ($approvals -ne "(None)") {
+                        $approvals += "(None)"
+                    }
                 }
             }
-            else {
+
+            if ($approvals -eq "") {
                 $approvals += "(None)"
             }
-        }
 
-        if ($approvals -eq "") {
-            $approvals += "(None)"
-        }
+            ##CreatedOn control
+            $createdOn = ""
+            if ($stage.createdOn) {
+                 $createdOn += $stage.createdOn
+            }
+            else {
+                $createdOn += "(None)"
+            }
 
-        ##CreatedOn control
-        $createdOn = ""
-        if ($stage.createdOn) {
-             $createdOn += $stage.createdOn
-        }
-        else {
-            $createdOn += "(None)"
-        }
+            $properties = [ordered]@{
+              Pipeline    = $release.releaseDefinition.name
+              Release     = $release.name
+              ReleaseId     = $release.id
+              Stage = $stage.name
+              StageStatus = $stage.status
+              StageId = $stage.id
+              CreatedOn = $createdOn
+              Approvals = $approvals
+            }
 
-        $properties = [ordered]@{
-          Pipeline    = $release.releaseDefinition.name
-          Release     = $release.name
-          ReleaseId     = $release.id
-          Stage = $stage.name
-          StageStatus = $stage.status
-          StageId = $stage.id
-          CreatedOn = $createdOn
-          Approvals = $approvals
+            $customObject = New-Object -TypeName PSCustomObject -Property $properties
+
+            $results += $customObject
         }
-
-        $customObject = New-Object -TypeName PSCustomObject -Property $properties
-
-        $results += $customObject
     }
 }
+while($continuationToken)
 
 
 ## Order the releases
@@ -80,15 +130,14 @@ $results = $results | Sort-Object -Property @{Expression = "Pipeline"},
                                             @{Expression = "ReleaseId"; Descending = $False},
                                             @{Expression = "StageId"; Descending = $False}
 
-## Filter by pipeline name
-$resultsFiltered = $results | ?{ $_.Pipeline -eq "New release pipeline" }
-
 ## File creation
+Write-Host "Creating file..."
+
 $date = Get-Date -Format G
 $dateFormated = $date.Replace("/", "-").Replace(":", ".")
 $fileName = "$pipeline Releases $dateFormated.csv"
 
-$resultsFiltered | Select `
+$results | Select `
             Pipeline,
             ReleaseId,
             Release,
@@ -96,3 +145,5 @@ $resultsFiltered | Select `
             StageStatus,
             CreatedOn,
             Approvals | export-csv -Path $fileName -NoTypeInformation
+
+Write-Host "File '$fileName' created."
